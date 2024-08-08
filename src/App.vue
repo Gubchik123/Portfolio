5fa3ad43e15512389a8f8afbb1c984495d967c1e
Nikita <nikita.gubarev31@gmail.com>
2024-08-08 14:52:10 +0300

    Update the structure to Vue.js to start V2

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url("./assets/style.css");
+</style>
